5787ca595f9aac80567ab1caba35e5a568fed983
loudsheep <loudsheep05@gmail.com>
2022-10-01 12:26:13 +0200

    Added 404 error if user does not exist

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         </div>
         <div class="col-9 pt-5">
             <div>
-                <h1> {{ $user->usernameProfile }} </h1>
+                <h1> {{ $user->username }} </h1>
             </div>
             <div class="d-flex">
                 <div class="pe-5"><strong>156</strong> posts</div>
