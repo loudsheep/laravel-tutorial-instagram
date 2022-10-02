2a2479e69ae0eeee76292e0c905967938a728927
loudsheep <loudsheep05@gmail.com>
2022-10-02 17:32:25 +0200

    Fixed post create route problem

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Auth::routes();
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 
-Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
+Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
