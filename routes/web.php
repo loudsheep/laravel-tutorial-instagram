bcc29a1b288f9d9923baf1e70fe1b07c772c5b75
loudsheep <loudsheep05@gmail.com>
2022-10-06 16:53:08 +0200

    Changed home route after login to latest posts

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
@@ -25,6 +22,7 @@ Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, '
 Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.update');
 Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
 
+Route::get('/', [App\Http\Controllers\PostsController::class, 'index']);
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
 Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
