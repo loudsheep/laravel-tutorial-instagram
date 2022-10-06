df96f7c683dcccdfbc40bd05b1dcb1c05fcbc9e8
loudsheep <loudsheep05@gmail.com>
2022-10-06 17:25:30 +0200

    Added email on register to users

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Welcome to this website
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo delectus eos mollitia tenetur! Saepe ipsam ipsa et recusandae ab ea sit totam distinctio, maiores perferendis itaque enim, id quo. A!
+
+Thanks,
+Automated email system
+@endcomponent
